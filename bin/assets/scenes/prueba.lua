840b2fd51a8a8cb490e660e2c1ed0d7a9ac0325a
likang03 <likangwu@ucm.es>
2024-03-15 01:51:53 +0100

    probar escena de unity

---
+++ bin/assets/scenes/prueba.lua
@@ -0,0 +1,64 @@
+scene  = {
+		{
+			components = {
+				Transform = {
+					positionX = 0.0,
+					positionY = 0.0,
+					positionZ = 0.0,
+					scaleX = 20.0,
+					scaleY = 1.0,
+					scaleZ = 15.0,
+					rotationX = 0.0,
+					rotationY = 0.0,
+					rotationZ = 0.0
+				},
+				RigidBody = {
+					colliderScaleX = 20.0,
+					colliderScaleY = 1.0,
+					colliderScaleZ = 15.0,
+					isTrigger = false,
+					mass = 1.0,
+					friction = 1.0,
+					damping = 0.4,
+					bounciness = 0.0,
+					colShape = 0,
+					movementType = 1
+				},
+				MeshRenderer = {
+					meshName  = "meshes/cube2.mesh",
+					materialName = "white"
+				}
+			}
+		},
+		{
+			components = {
+				Transform = {
+					positionX = -9.06,
+					positionY = 7.97,
+					positionZ = 0.0,
+					scaleX = 1.0,
+					scaleY = 1.0,
+					scaleZ = 1.0,
+					rotationX = 0.0,
+					rotationY = 0.0,
+					rotationZ = 0.0
+				},
+				RigidBody = {
+					colliderScaleX = 1.0,
+					colliderScaleY = 1.0,
+					colliderScaleZ = 1.0,
+					isTrigger = false,
+					mass = 1.0,
+					friction = 1.0,
+					damping = 0.4,
+					bounciness = 0.0,
+					colShape = 0,
+					movementType = 1
+				},
+				MeshRenderer = {
+					meshName  = "meshes/cube2.mesh",
+					materialName = "white"
+				}
+			}
+		}
+}
