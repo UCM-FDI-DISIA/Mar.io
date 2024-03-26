2b8d3ff685327725a6ae0f4fdbe87d1873063bfe
pedroleo <pedroleo@ucm.es>
2024-03-26 08:43:49 +0100

    Camara en Mar.io

--- bin/assets/scenes/GameMap1.lua
+++ bin/assets/scenes/GameMap1.lua
@@ -1,4 +1,25 @@
-scene  = {
+scene = {
+	{
+		components = {
+			Transform = {
+				positionX = 50.0,
+				positionY = 50.0,
+				positionZ = 50.0,
+				scaleX = 1.0,
+				scaleY = 1.0,
+				scaleZ = 1.0,
+				rotationX = 0.0,
+				rotationY = 0.0,
+				rotationZ = 0.0
+			},
+			CameraComponent = {
+				zOrder = 0,
+				bgColorR = 0.925,
+				bgColorG = 0.698,
+				bgColorB = 0.941
+			}
+		}
+	},
     {
         components={
 			Transform = {
