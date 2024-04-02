980383bcab949ccd8bc36b88ad7d55589deae52c
MattCastUCM <mattcast@ucm.es>
2024-04-02 10:31:52 +0200

    Olovorgo las cosas que no usábamos

--- bin/assets/scenes/GameMap1.lua
+++ bin/assets/scenes/GameMap1.lua
@@ -13,7 +13,7 @@ scene = {
 				rotationZ = 0.0
 			},
 			MeshRenderer = {
-				meshName = "corals/Coral1.mesh"
+				meshName = "models/corals/Coral1.mesh"
 			},
 			RigidBody = {  
 				colShape = 0, --0 BOX, 1 SPHERE, 2 PLANE, 3 CAPSULE
@@ -64,8 +64,8 @@ scene = {
 			}
 		}
 	},
-  {
-    components = {
+	{
+		components = {
 			Transform = {
 				positionX = 10.0,
 				positionY = 10.0,
@@ -78,7 +78,7 @@ scene = {
 				rotationZ = 0.0
 			},
 			MeshRenderer = {  
-				meshName = "meshes/Cofre1.mesh",
+				meshName = "models/chest/chest1.mesh",
 			},
 			RigidBody = {  
 				colShape = 0, --0 BOX, 1 SPHERE, 2 PLANE, 3 CAPSULE
@@ -99,238 +99,237 @@ scene = {
   	 	}
 	},
 	Player = {
-			components = {
-				Transform = {
-					positionX = 0.0,
-					positionY = 3.0,
-					positionZ = 0.0,
-					scaleX = 2.0,
-					scaleY = 2.0,
-					scaleZ = 2.0,
-					rotationX = 0.0,
-					rotationY = 180.0,
-					rotationZ = 0.0
-				},
-				MeshRenderer = {
-					meshName = "racoon/pirateRaccoon/PirateRacoon.mesh",
-					initRotationY = 0.0
-				},
-				RigidBody = {  
-					colShape = 3, --0 BOX, 1 SPHERE, 2 PLANE, 3 CAPSULE
-					colliderScaleX = 2.0,
-					colliderScaleY = 5.0,
-					colliderScaleZ = 2.0,
-					isTrigger = false,
-					movementType = 0, --0 DYNAMIC , 1 STATIC, 2 KINEMATIC
-					mass = 3.0,
-					friction = 1.0,
-					damping = 0.4,
-					bounciness = 2.0
-				},
-				Collider = {},
-				PlayerMovementController = {
-
-				},
-				PlayerSetting = {},
-				HealthComponent = {
-					maxHealth = 2,
-					currHealth = 1,
-					gracePeriod = 1.0
-				}
+		components = {
+			Transform = {
+				positionX = 0.0,
+				positionY = 3.0,
+				positionZ = 0.0,
+				scaleX = 2.0,
+				scaleY = 2.0,
+				scaleZ = 2.0,
+				rotationX = 0.0,
+				rotationY = 180.0,
+				rotationZ = 0.0
 			},
-			children = {
-				{
-					components = {
-						Transform = {
-							positionX = 0.0,
-							positionY = 0.0,
-							positionZ = 5.0,
-							scaleX = 1.0,
-							scaleY = 1.0,
-							scaleZ = 1.2,
-							rotationX = 0.0,
-							rotationY = 0.0,
-							rotationZ = 0.0
-						},
-						RigidBody = {
-							colliderScaleX = 2.0,
-							colliderScaleY = 2.0,
-							colliderScaleZ = 2.0,
-							isTrigger = true,
-							mass = 0.0,
-							friction = 1.0,
-							damping = 0.4,
-							bounciness = 0.0,
-							colShape = 0,
-							movementType = 2
-						}
-						-- ,
-						-- MeshRenderer = {
-						-- 	meshName  = "meshes/cube2.mesh",
-						-- }
-						,
-						FistComponent = {
-							coolDown = 1000.0,
-							duration = 100.0
-						}
+			MeshRenderer = {
+				meshName = "models/pirateRaccoon/PirateRacoon.mesh",
+				initRotationY = 0.0
+			},
+			RigidBody = {  
+				colShape = 3, --0 BOX, 1 SPHERE, 2 PLANE, 3 CAPSULE
+				colliderScaleX = 2.0,
+				colliderScaleY = 5.0,
+				colliderScaleZ = 2.0,
+				isTrigger = false,
+				movementType = 0, --0 DYNAMIC , 1 STATIC, 2 KINEMATIC
+				mass = 3.0,
+				friction = 1.0,
+				damping = 0.4,
+				bounciness = 2.0
+			},
+			Collider = {},
+			PlayerMovementController = {
+			
+			},
+			PlayerSetting = {},
+			HealthComponent = {
+				maxHealth = 2,
+				currHealth = 1,
+				gracePeriod = 1.0
+			}
+		},
+		children = {
+			{
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 5.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.2,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					RigidBody = {
+						colliderScaleX = 2.0,
+						colliderScaleY = 2.0,
+						colliderScaleZ = 2.0,
+						isTrigger = true,
+						mass = 0.0,
+						friction = 1.0,
+						damping = 0.4,
+						bounciness = 0.0,
+						colShape = 0,
+						movementType = 2
+					}
+					-- ,
+					-- MeshRenderer = {
+					-- 	meshName  = "models/cube2.mesh",
+					-- }
+					,
+					FistComponent = {
+						coolDown = 1000.0,
+						duration = 100.0
 					}
 				}
 			}
-
-		},
-		{
-			components = {
-				Transform = {
-					positionX = 0.0,
-					positionY = 0.0,
-					positionZ = 0.0,
-					scaleX = 30.0,
-					scaleY = 1.0,
-					scaleZ = 30.0,
-					rotationX = 0.0,
-					rotationY = 0.0,
-					rotationZ = 0.0
-				},
-				RigidBody = {
-					colliderScaleX = 30.0,
-					colliderScaleY = 1.0,
-					colliderScaleZ = 30.0,
-					isTrigger = false,
-					mass = 1.0,
-					friction = 1.0,
-					damping = 0.4,
-					bounciness = 0.0,
-					colShape = 0,
-					movementType = 1
-				},
-				MeshRenderer = {
-					meshName  = "meshes/cube2.mesh",
-					materialName = "white"
-				}
-			}
-		},
-		{
-			components = {
-				Transform = {
-					positionX = 0.0,
-					positionY = 14.89,
-					positionZ = -33.52,
-					scaleX = 20.0,
-					scaleY = 1.0,
-					scaleZ = 15.0,
-					rotationX = 0.0,
-					rotationY = 0.0,
-					rotationZ = 0.0
-				},
-				RigidBody = {
-					colliderScaleX = 20.0,
-					colliderScaleY = 1.0,
-					colliderScaleZ = 15.0,
-					isTrigger = false,
-					mass = 1.0,
-					friction = 1.0,
-					damping = 0.4,
-					bounciness = 0.0,
-					colShape = 0,
-					movementType = 1
-				},
-				MeshRenderer = {
-					meshName  = "meshes/cube2.mesh",
-					materialName = "white"
-				}
-			}
-		},
-		{
-			components = {
-				Transform = {
-					positionX = 7.2,
-					positionY = 2.34,
-					positionZ = -10.4,
-					scaleX = 4.0,
-					scaleY = 1.0,
-					scaleZ = 2.0,
-					rotationX = 0.0,
-					rotationY = 0.0,
-					rotationZ = 0.0
-				},
-				RigidBody = {
-					colliderScaleX = 4.0,
-					colliderScaleY = 1.0,
-					colliderScaleZ = 2.0,
-					isTrigger = false,
-					mass = 1.0,
-					friction = 1.0,
-					damping = 0.4,
-					bounciness = 0.0,
-					colShape = 0,
-					movementType = 1
-				},
-				MeshRenderer = {
-					meshName  = "meshes/cube2.mesh",
-					materialName = "white"
-				}
-			}
-		},
-		{
-			components = {
-				Transform = {
-					positionX = -3.23,
-					positionY = 7.9,
-					positionZ = -10.4,
-					scaleX = 4.0,
-					scaleY = 1.0,
-					scaleZ = 2.0,
-					rotationX = 0.0,
-					rotationY = 0.0,
-					rotationZ = 0.0
-				},
-				RigidBody = {
-					colliderScaleX = 4.0,
-					colliderScaleY = 1.0,
-					colliderScaleZ = 2.0,
-					isTrigger = false,
-					mass = 1.0,
-					friction = 1.0,
-					damping = 0.4,
-					bounciness = 0.0,
-					colShape = 0,
-					movementType = 1
-				},
-				MeshRenderer = {
-					meshName  = "meshes/cube2.mesh",
-					materialName = "white"
-				}
-			}
-		},
-		{
-			components = {
-				Transform = {
-					positionX = 7.2,
-					positionY = 11.3,
-					positionZ = -16.28,
-					scaleX = 4.0,
-					scaleY = 1.0,
-					scaleZ = 2.0,
-					rotationX = 0.0,
-					rotationY = 0.0,
-					rotationZ = 0.0
-				},
-				RigidBody = {
-					colliderScaleX = 4.0,
-					colliderScaleY = 1.0,
-					colliderScaleZ = 2.0,
-					isTrigger = false,
-					mass = 1.0,
-					friction = 1.0,
-					damping = 0.4,
-					bounciness = 0.0,
-					colShape = 0,
-					movementType = 1
-				},
-				MeshRenderer = {
-					meshName  = "meshes/cube2.mesh",
-					materialName = "white"
-				}
+		}
+	},
+	{
+		components = {
+			Transform = {
+				positionX = 0.0,
+				positionY = 0.0,
+				positionZ = 0.0,
+				scaleX = 30.0,
+				scaleY = 1.0,
+				scaleZ = 30.0,
+				rotationX = 0.0,
+				rotationY = 0.0,
+				rotationZ = 0.0
+			},
+			RigidBody = {
+				colliderScaleX = 30.0,
+				colliderScaleY = 1.0,
+				colliderScaleZ = 30.0,
+				isTrigger = false,
+				mass = 1.0,
+				friction = 1.0,
+				damping = 0.4,
+				bounciness = 0.0,
+				colShape = 0,
+				movementType = 1
+			},
+			MeshRenderer = {
+				meshName  = "models/cube2.mesh",
+				materialName = "white"
 			}
 		}
+	},
+	{
+		components = {
+			Transform = {
+				positionX = 0.0,
+				positionY = 14.89,
+				positionZ = -33.52,
+				scaleX = 20.0,
+				scaleY = 1.0,
+				scaleZ = 15.0,
+				rotationX = 0.0,
+				rotationY = 0.0,
+				rotationZ = 0.0
+			},
+			RigidBody = {
+				colliderScaleX = 20.0,
+				colliderScaleY = 1.0,
+				colliderScaleZ = 15.0,
+				isTrigger = false,
+				mass = 1.0,
+				friction = 1.0,
+				damping = 0.4,
+				bounciness = 0.0,
+				colShape = 0,
+				movementType = 1
+			},
+			MeshRenderer = {
+				meshName  = "models/cube2.mesh",
+				materialName = "white"
+			}
+		}
+	},
+	{
+		components = {
+			Transform = {
+				positionX = 7.2,
+				positionY = 2.34,
+				positionZ = -10.4,
+				scaleX = 4.0,
+				scaleY = 1.0,
+				scaleZ = 2.0,
+				rotationX = 0.0,
+				rotationY = 0.0,
+				rotationZ = 0.0
+			},
+			RigidBody = {
+				colliderScaleX = 4.0,
+				colliderScaleY = 1.0,
+				colliderScaleZ = 2.0,
+				isTrigger = false,
+				mass = 1.0,
+				friction = 1.0,
+				damping = 0.4,
+				bounciness = 0.0,
+				colShape = 0,
+				movementType = 1
+			},
+			MeshRenderer = {
+				meshName  = "models/cube2.mesh",
+				materialName = "white"
+			}
+		}
+	},
+	{
+		components = {
+			Transform = {
+				positionX = -3.23,
+				positionY = 7.9,
+				positionZ = -10.4,
+				scaleX = 4.0,
+				scaleY = 1.0,
+				scaleZ = 2.0,
+				rotationX = 0.0,
+				rotationY = 0.0,
+				rotationZ = 0.0
+			},
+			RigidBody = {
+				colliderScaleX = 4.0,
+				colliderScaleY = 1.0,
+				colliderScaleZ = 2.0,
+				isTrigger = false,
+				mass = 1.0,
+				friction = 1.0,
+				damping = 0.4,
+				bounciness = 0.0,
+				colShape = 0,
+				movementType = 1
+			},
+			MeshRenderer = {
+				meshName  = "models/cube2.mesh",
+				materialName = "white"
+			}
+		}
+	},
+	{
+		components = {
+			Transform = {
+				positionX = 7.2,
+				positionY = 11.3,
+				positionZ = -16.28,
+				scaleX = 4.0,
+				scaleY = 1.0,
+				scaleZ = 2.0,
+				rotationX = 0.0,
+				rotationY = 0.0,
+				rotationZ = 0.0
+			},
+			RigidBody = {
+				colliderScaleX = 4.0,
+				colliderScaleY = 1.0,
+				colliderScaleZ = 2.0,
+				isTrigger = false,
+				mass = 1.0,
+				friction = 1.0,
+				damping = 0.4,
+				bounciness = 0.0,
+				colShape = 0,
+				movementType = 1
+			},
+			MeshRenderer = {
+				meshName  = "models/cube2.mesh",
+				materialName = "white"
+			}
+		}
+	}
 }
