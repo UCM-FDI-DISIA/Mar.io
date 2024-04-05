a50cf5ad7bc3fef0074cd1a43db1305120b73d0c
likangwu03 <likangwu@ucm.es>
2024-04-05 10:10:15 +0200

    GameManager de otro repo

---
+++ src/Mar.io/Components/GameManager.cpp
@@ -0,0 +1,113 @@
+#include "GameManager.h"
+#include "SceneManager.h"
+
+
+#include <iomanip>
+
+template class JUEGO_API Tapioca::Singleton<MarIo::GameManager>;
+
+template<>
+MarIo::GameManager* Tapioca::Singleton<MarIo::GameManager>::instance_ = nullptr;
+
+namespace MarIo {
+
+GameManager::GameManager() : INIT_TIME(0), INIT_LIFE(0), time(0), life(0), state(MainMenu), score(0) { }
+
+bool initComponent(const CompMap& variables) { return false; }
+
+bool GameManager::initComponent(const CompMap& variables) {
+
+    bool initLifeSet = setValueFromMap(INIT_LIFE, "initLife", variables);
+    if (!initLifeSet) {
+#ifdef _DEBUG
+        std::cerr << "Error: GameManager: no se pudo inicializar INIT_LIFE.\n";
+#endif
+        return false;
+    }
+    float timeAux;
+    bool initTimeSet = setValueFromMap(timeAux, "initTime", variables);
+    if (!initTimeSet) {
+#ifdef _DEBUG
+        std::cerr << "Error: GameManager: no se pudo inicializar INIT_TIME.\n";
+#endif
+        return false;
+    }
+    INIT_TIME = (long long int)timeAux;
+    // create();
+    onStart();
+
+    return true;
+}
+
+void GameManager::start() { state = InGame; }
+
+void GameManager::update(const uint64_t deltaTime) {
+    if (state == InGame) {
+        time -= deltaTime;
+#ifdef _DEBUG
+        //std::cout << std::fixed << std::setprecision(2) << time / 1000. << "\n";
+#endif
+        if (time <= 0) {
+            state = Pause;
+            changeScene("PauseMenu.lua");
+#ifdef _DEBUG 
+            std::cout << "El jugador se ha quedado sin tiempo.\n";
+#endif
+            pushEvent("ev_GameOver", nullptr);
+        }
+    }
+}
+
+void GameManager::handleEvent(std::string const& id, void* info) {
+    if (id == "ev_Pause") {
+        changeScene("PauseMenu.lua");
+    }
+    if (id == "ev_GameOver") {
+        onGameOver();
+    }
+}
+
+void GameManager::onStart() {
+    time = INIT_TIME * 1000;
+    score = 0;
+    life = INIT_LIFE;
+}
+
+void GameManager::onGameOver() { }
+
+void GameManager::onWin() { }
+
+int GameManager::getScore() { return score; }
+
+int GameManager::getLife() { return life; }
+
+uint64_t GameManager::getTime() { return (uint64_t)(time / 1000.f); }
+
+bool GameManager::changeScene(std::string const& scene) const {
+    return Tapioca::SceneManager::instance()->loadScene(scene);
+}
+
+void GameManager::setScore(const int s) { score = s; }
+
+void GameManager::setLife(const int l) { life = l; }
+
+void GameManager::setTime(uint64_t t) { time = t * 1000; }
+
+void GameManager::changeScore(int s) { score += s; }
+
+void GameManager::changeLife(int l) { life += l; }
+
+void GameManager::changeTime(uint64_t t) { time += t * 1000; }
+
+void GameManager::loseLife() {
+    if (life > 0) {
+        life--;
+    }
+    else {
+#ifdef _DEBUG
+        std::cout << "El jugador se ha quedado sin vidas.\n";
+#endif
+        pushEvent("ev_GameOver", nullptr);
+    }
+}
+}
---
+++ src/Mar.io/Components/GameManager.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "Structure/Component.h"
+#include "Utilities/Singleton.h"
+#include "Structure/ComponentBuilder.h"
+#include "../gameDefs.h"
+#include <string>
+
+namespace MarIo {
+class JUEGO_API GameManager : public Tapioca::Component, public Tapioca::Singleton<GameManager> {
+private:
+    friend Singleton<GameManager>;
+
+    enum State { MainMenu, InGame, GameOver, Pause };
+
+    State state;
+
+    //al inicializarse
+    int INIT_LIFE;
+    int64_t INIT_TIME;
+    //static GameManager* instance_;
+
+    //actuales
+    int score;
+    int life;
+    int64_t time;
+
+    GameManager();
+
+    void onStart();
+    void onGameOver();
+    void onWin();
+
+    //    static GameManager* create() {
+    //        //assert(instance_.get() == nullptr, "Instance already exists");
+    //        if (instance_ == nullptr) instance_ = new GameManager();
+    //#ifdef _DEBUG
+    //        else
+    //            std::cout << "Instance already exists\n";
+    //#endif
+    //        return instance_;
+    //    }
+
+public:
+    COMPONENT_ID("GameManager");
+
+    GameManager(GameManager&) = delete;
+    GameManager(GameManager&&) = delete;
+    GameManager& operator=(GameManager&) = delete;
+    GameManager& operator=(GameManager&&) = delete;
+
+    bool initComponent(const CompMap& variables) override;
+    void start() override;
+    void update(const uint64_t deltaTime) override;
+    void handleEvent(std::string const& id, void* info) override;
+
+    int getScore();
+    int getLife();
+    uint64_t getTime();
+
+    bool changeScene(std::string const& scene) const;
+
+    //setear valor absoluto de puntuación
+    void setScore(const int s);
+    //setear valor absoluto de vida
+    void setLife(const int l);
+    //setear valor absoluto de tiempo
+    void setTime(const uint64_t t);
+
+    //incrementar/decrementar x cantidad de puntuación
+    void changeScore(const int s);
+    //incrementar/decrementar x cantidad de vida
+    void changeLife(const int l);
+    //incrementar/decrementar x cantidad de tiempo
+    void changeTime(const uint64_t t);
+
+    //resta una vida a la vida total del jugador
+    void loseLife();
+};
+
+class JUEGO_API GameManagerBuilder : public Tapioca::ComponentBuilder {
+public:
+    GameManagerBuilder() : Tapioca::ComponentBuilder(GameManager::id) { }
+
+    inline Tapioca::Component* createComponent() override { return GameManager::create(); }
+};
+}
