076baeab8cfd96f47cd7458e3c939055938b58ab
MattCastUCM <mattcast@ucm.es>
2024-05-03 16:06:10 +0200

    Más avances en el mapa

--- bin/assets/scenes/GameMap.lua
+++ bin/assets/scenes/GameMap.lua
@@ -2,15 +2,15 @@ scene  = {
 	Camera = {
 		components = {
 			Transform = {
-				positionX = -2.5,
-				positionY = 27.5,
-				positionZ = 1.3,
+				positionX = 0.0,
+				positionY = 0.0,
+				positionZ = -10.0,
 				scaleX = 1.0,
 				scaleY = 1.0,
 				scaleZ = 1.0,
-				rotationX = 00.0,
+				rotationX = 0.0,
 				rotationY = 0.0,
-				rotationZ = 00.0
+				rotationZ = 0.0
 			},
 			CameraComponent = {
 				zOrder = 0,
@@ -28,7 +28,7 @@ scene  = {
 			}
 		}
 	},
-	{
+	Light = {
 		components = {
 			Transform = {
 				positionX = 0.0,
@@ -42,21 +42,19 @@ scene  = {
 				rotationZ = 0.0
 			},
 			LightDirComp = {
-				directionX = 0.0,
+				directionX = 1.0,
 				directionY = -1.0,
 				directionZ = -1.0,
 				mainLight = true
 			}
 		}
 	},
-
-	
 	Player = {
 		components = {
 			Transform = {
-				positionX = -2.29,
+				positionX = 0,
 				positionY = 27.45,
-				positionZ = 1.29,
+				positionZ = -5.0,
 				scaleX = 1.0,
 				scaleY = 1.6,
 				scaleZ = 1.0,
@@ -67,7 +65,6 @@ scene  = {
 			MeshRenderer = {
 				meshName  = "models/1mCube.mesh",
 				materialName = "white",
-				initRotationY = 0.0
 			},
 			RigidBody = {
 				colliderScaleX = 0.5,
@@ -95,7 +92,7 @@ scene  = {
 		}
 	},
 
-	{
+	Scenario = {
 		components = {
 			Transform = {
 				positionX = 0.0,
@@ -105,62 +102,193 @@ scene  = {
 				scaleY = 1.0,
 				scaleZ = 1.0,
 				rotationX = 0.0,
-				rotationY = 0.0,
+				rotationY = 90.0,
 				rotationZ = 0.0
-			},
-			
+			}
 		},
 		children = {
-			{
+			Corals = {
 				components = {
 					Transform = {
-						positionX = -27.06171,
-						positionY = 26.58034,
-						positionZ = 0.69,
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
 						scaleX = 1.0,
 						scaleY = 1.0,
 						scaleZ = 1.0,
 						rotationX = 0.0,
-						rotationY = 90.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Corals.mesh"
+					},
+				},
+			},
+			Seaweed = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Seaweed.mesh",
+					},
+				},
+			},
+			Background = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Background.mesh",
+					},
+				},
+			},
+			
+			Platform1 = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
 						rotationZ = 0.0
 					},
 					MeshRenderer = {
 						meshName  = "models/scenario/Platform1.mesh",
-						initRotationY = 90.0
 					},
 				},
-				children = {
-					{
-						components = {
-							Transform = {
-								positionX = -7.79,
-								positionY = 21.29,
-								positionZ = 0.69,
-								scaleX = 23.299,
-								scaleY = 7.98,
-								scaleZ = 17.44,
-								rotationX = 0.0,
-								rotationY = 0.0,
-								rotationZ = 0.0
-							},
-							RigidBody = {
-								colliderScaleX = 0.5,
-								colliderScaleY = 0.5,
-								colliderScaleZ = 0.5,
-								isTrigger = false,
-								mass = 1.0,
-								friction = 1.0,
-								damping = 0.4,
-								bounciness = 0.0,
-								colShape = 0,
-								movementType = 1
-							}
-						}
-					}
+			},
+
+			Platform2 = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Platform2.mesh",
+					},
 				},
 			},
-		},
-	},
+
+			Platform3 = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Platform3.mesh",
+					},
+				},
+			},
+
+			Rocks = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Rocks.mesh",
+					},
+				},
+			},
+
+			Stairs = {
+				components = {
+					Transform = {
+						positionX = 0.0,
+						positionY = 0.0,
+						positionZ = 0.0,
+						scaleX = 1.0,
+						scaleY = 1.0,
+						scaleZ = 1.0,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					MeshRenderer = {
+						meshName  = "models/scenario/Stairs.mesh",
+					},
+				},
+			},
+
+
+			Collider1 = {
+				components = {
+					Transform = {
+						positionX = 6.31,
+						positionY = 22.37,
+						positionZ = 0.0,
+						scaleX = 22.37,
+						scaleY = 7.98,
+						scaleZ = 17.44,
+						rotationX = 0.0,
+						rotationY = 0.0,
+						rotationZ = 0.0
+					},
+					RigidBody = {
+						colliderScaleX = 0.5,
+						colliderScaleY = 0.5,
+						colliderScaleZ = 0.5,
+						isTrigger = false,
+						mass = 1.0,
+						friction = 1.0,
+						damping = 0.4,
+						bounciness = 0.0,
+						colShape = 0,
+						movementType = 1
+					}
+				}
+			}
+
+		}
+	}
+	
 
 	
 
